a9f9bd4613e6e5ba72b25deaba5bd61ff7a598f1
Thijs Miedema <t.miedema@live.nl>
2017-11-22 20:51:52 +0100

    Finalized Conscribo PyAPI

--- tester.py
+++
@@ -1,9 +0,0 @@
-from mapper import TransactionResult
-
-
-with open("test.xml", "rb") as f:
-    xml = f.read()
-    
-res = TransactionResult(xml)
-print(res.success)
-print(res.transactions[0].rows)
